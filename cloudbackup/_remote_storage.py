1a560222740030506969599c4401dbec34b8d5b5
Dmitry Podaruev <dmitry.podaruev@urfu.me>
2020-11-03 10:30:40 +0500

    switch from `while True` to `for` loop

--- cloudbackup/_remote_storage.py
+++ cloudbackup/_remote_storage.py
@@ -21,21 +21,17 @@ class RemoteStorage:
                 uploaded_files.append(res)
             elif os.path.isdir(file_abs_path):
                 parents = {}
-                while True:
-                    try:
-                        root, dirs, filenames = next(tree)
-                        parent_id = parents[os.path.split(root)[0]] if parents else []
-                        # os.path.split returns pair (head, tail) of path
-                        folder_id = self.gdrive.create_folder(os.path.split(root)[-1],
-                                                              parent_id=parent_id)
-                        if not filenames:
-                            continue
-                        for file in filenames:
-                            res = gdrive_upload(os.path.join(root, file), parent_id=folder_id)
-                            uploaded_files.append(res)
-                        parents[root] = folder_id
-                    except StopIteration:
-                        break
+                for root, dirs, filenames in tree:
+                    parent_id = parents[os.path.split(root)[0]] if parents else []
+                    # os.path.split returns pair (head, tail) of path
+                    folder_id = self.gdrive.create_folder(os.path.split(root)[-1],
+                                                          parent_id=parent_id)
+                    if not filenames:
+                        continue
+                    for file in filenames:
+                        res = gdrive_upload(os.path.join(root, file), parent_id=folder_id)
+                        uploaded_files.append(res)
+                    parents[root] = folder_id
             else:
                 print("This directory or file doesn\'t exists")
         return uploaded_files
