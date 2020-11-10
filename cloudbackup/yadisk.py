c06acc2afa511b5e75ab93c0a519a01bc3e1bed9
Dmitry Podaruev <dmitry.podaruev@urfu.me>
2020-11-11 00:06:23 +0500

    Implement download file and improve list files

--- cloudbackup/yadisk.py
+++ cloudbackup/yadisk.py
@@ -2,6 +2,7 @@ import json
 import os
 import mimetypes
 import requests
+from urllib.parse import parse_qs
 from ._yadisk_authenticator import YaDiskAuth
 
 
@@ -13,7 +14,7 @@ class YaDisk:
         }
         self.all_files = self._get_files()
 
-    def _get_files(self, path=None, trashed=False, list_all=False):
+    def _get_files(self, path=None):
         """
         Process the raw response for file meta-information
         :param path:
@@ -21,34 +22,47 @@ class YaDisk:
         :param list_all:
         :return:
         """
-
-        files = []
         if path is not None:
             r = requests.get("https://cloud-api.yandex.net/v1/disk/resources/", params={"path": path},
                              headers=self.auth_headers).json()
-            files_list = r["_embedded"]["items"]
+            try:
+                files_list = r["_embedded"]["items"]
+            except KeyError:
+                files_list = r
         else:
             r = requests.get("https://cloud-api.yandex.net/v1/disk/resources/files", headers=self.auth_headers).json()
             files_list = r["items"]
-        for file_data in files_list:
-            file = {}
-            for key in file_data:
-                if key == "name" or key == "path" or key == "type" or key == "mime_type":
-                    file[key] = file_data[key]
-                elif key == "resource_id":
-                    file["id"] = file_data[key].split(":")[1]
-                    # resource id is 115807909:3c36364d42da9c... where first part is not unique
-            files.append(file)
-        return files
+        return files_list
 
     def list_files(self, folder_id=None) -> list:
         """
         Return list of dicts: {'name': '', 'id': '', 'path': '', 'type': '', 'mime_type': ''}
         'type' not equals 'mimeType', 'type' might be only 'dir' or 'file'
         """
+        files = []
         if folder_id == "" or folder_id is None:
-            return self.all_files
+            for file_data in self._get_files():
+                file = {}
+                for key in file_data:
+                    if key == "name" or key == "path" or key == "type" or key == "mime_type":
+                        file[key] = file_data[key]
+                    elif key == "resource_id":
+                        file["id"] = file_data[key].split(":")[1]
+                        # resource id is 115807909:3c36364d42da9c... where first part is not unique
+                files.append(file)
+            return files
         return self._get_files(path=folder_id)
 
-    def _get_path_by_id(self):
-        pass
+    def get_path_by_id(self, id):
+        for file in self.all_files:
+            for metainf in file:
+                if file[metainf].startswith(id):
+                    return file["path"]
+
+    def download(self, path):
+        r = requests.get("https://cloud-api.yandex.net/v1/disk/resources/download", headers=self.auth_headers,
+                         params={"path": path}).json()
+        parsed_url = parse_qs(r["href"])  # returns queries like 'filename': ['test.zip']
+        download_request = requests.get(r["href"])
+        with open(parsed_url["filename"][0], "wb+") as f:
+            f.write(download_request.content)
