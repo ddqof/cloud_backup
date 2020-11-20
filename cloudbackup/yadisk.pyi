f7f2eda65346cfd927818967b3169c2ae21cc61a
Dmitry Podaruev <dmitry.podaruev@urfu.me>
2020-11-20 20:10:03 +0500

    Implement type hinting for yadisk and gdrive

---
+++ cloudbackup/yadisk.pyi
@@ -0,0 +1,39 @@
+from ._authenticators import YaDiskAuth as YaDiskAuth
+from ._exceptions import ApiResponseException as ApiResponseException, IncorrectPathException as IncorrectPathException
+from ._file_objects import YaDiskFile as YaDiskFile
+import requests
+
+
+class YaDisk:
+    _auth_headers: dict
+    def __init__(self) -> None: ...
+    def lsdir(self,
+              directory: str,
+              sort: str = ...,
+              limit: int = ...
+    ) -> list: ...
+    def list_files(self,
+                   sort: str = ...,
+                   limit: int = ...
+    ) -> list: ...
+    def download(self,
+                 path: str
+    ) -> None: ...
+    def upload(self,
+               file_abs_path: str,
+               destination: str
+    ) -> None: ...
+    def _initilal_upload(self,
+                         destination: str
+    ) -> str: ...
+    def _single_upload(self,
+                       local_path: str,
+                       destination: str
+    ) -> None: ...
+    def mkdir(self,
+              destination: str
+    ) -> None: ...
+    @staticmethod
+    def _check_path(path: str) -> None: ...
+    @staticmethod
+    def _check_status(api_response: requests.models.Response) -> None: ...
