5c9916f284bf3b71493587693d6da4f6ee51a521
ddqof <dmitry.podaruev@urfu.me>
2021-02-03 18:36:21 +0500

    Change file, dir and g.suite names to [F], [D], [S] in defaults.py

--- defaults.py
+++ defaults.py
@@ -36,55 +36,55 @@ RM_ACCESS_DENIED_MSG = "No access was granted to remove"
 
 UPLOAD_COMPLETED_MSG = "Upload completed."
 DOWNLOAD_COMPLETED_MSG = "Download completed."
-SUCCESSFUL_DOWNLOAD_FILE_MSG = ("Successfully downloaded file:"
+SUCCESSFUL_DOWNLOAD_FILE_MSG = ("Successfully downloaded [F]:"
                                 " `{file_name}`")
-SUCCESSFUL_DOWNLOAD_DIR_MSG = ("Successfully downloaded directory:"
+SUCCESSFUL_DOWNLOAD_DIR_MSG = ("Successfully downloaded [D]:"
                                " `{dir_name}`")
-SUCCESSFUL_UPLOAD_FILE_MSG = ("Successfully uploaded file:"
+SUCCESSFUL_UPLOAD_FILE_MSG = ("Successfully uploaded [F]:"
                               " `{file_name}`")
-SUCCESSFUL_UPLOAD_DIR_MSG = ("Successfully uploaded directory:"
+SUCCESSFUL_UPLOAD_DIR_MSG = ("Successfully uploaded [D]:"
                              " `{dir_name}`")
-SUCCESSFUL_DELETE_FILE_MSG = ("Successfully deleted file:"
+SUCCESSFUL_DELETE_FILE_MSG = ("Successfully deleted [F]:"
                               " `{file_name}`")
-SUCCESSFUL_DELETE_DIR_MSG = ("Successfully deleted directory:"
+SUCCESSFUL_DELETE_DIR_MSG = ("Successfully deleted [D]:"
                              " `{dir_name}`")
-SUCCESSFUL_FILE_TRASH_MSG = ("Successfully moved file:"
+SUCCESSFUL_FILE_TRASH_MSG = ("Successfully moved [F]:"
                              " `{file_name}` to the trash")
-SUCCESSFUL_DIR_TRASH_MSG = ("Successfully moved directory:"
+SUCCESSFUL_DIR_TRASH_MSG = ("Successfully moved [D]:"
                             " `{dir_name}` to the trash")
 
-G_SUITE_FILE = "G.Suite file: `{file.name}` isn't downloadable."
+G_SUITE_FILE = "[S]: `{file.name}` isn't downloadable."
 
 MOVE_FILE_TO_TRASH_CONFIRMATION_MSG = ("Are you sure you want to"
-                                       " move file: `{file_name}`"
+                                       " move [F]: `{file_name}`"
                                        " to the trash? " +
                                        CONFIRM_CHOICE_STRING)
 MOVE_DIR_TO_TRASH_CONFIRMATION_MSG = ("Are you sure you want to"
-                                      " move directory: `{dir_name}`"
+                                      " move [D]: `{dir_name}`"
                                       " to the trash? " +
                                       CONFIRM_CHOICE_STRING)
 DELETE_DIR_CONFIRMATION_MSG = ("Are you sure you want to"
-                               " delete directory: `{dir_name}`? " +
+                               " delete [D]: `{dir_name}`? " +
                                CONFIRM_CHOICE_STRING)
 DELETE_FILE_CONFIRMATION_MSG = ("Are you sure you want to"
-                                " delete file: `{file_name}`? " +
+                                " delete [F]: `{file_name}`? " +
                                 CONFIRM_CHOICE_STRING)
 
 LIST_NEXT_PAGE_MSG = "List next page? " + CONFIRM_CHOICE_STRING
 
 OVERWRITE_FILE_REQUEST_MSG = ("Are you sure you want to"
-                              " overwrite file: `{file_name}`? " +
+                              " overwrite [F]: `{file_name}`? " +
                               CONFIRM_CHOICE_STRING)
 OVERWRITE_DIR_REQUEST_MSG = ("Are you sure you want to"
-                             " overwrite directory: `{dir_name}`? " +
+                             " overwrite [D]: `{dir_name}`? " +
                              CONFIRM_CHOICE_STRING)
-OVERWRITING_DIRECTORY_MSG = "Overwriting directory: `{dir_name}`..."
-OVERWRITING_FILE_MSG = "Overwriting file: `{file_name}`..."
+OVERWRITING_DIRECTORY_MSG = "Overwriting [D]: `{dir_name}`..."
+OVERWRITING_FILE_MSG = "Overwriting [F]: `{file_name}`..."
 
 MAKING_DIRECTORY_MSG = "Making directory: `{dir_name}`..."
-UPLOADING_DIRECTORY_MSG = "Uploading  dir: `{dir_name}`..."
-DOWNLOADING_FILE_MSG = "Downloading file: `{file_name}`..."
-UPLOADING_FILE_MSG = "Uploading file: `{file_name}`..."
-DOWNLOADING_DIR_AS_ZIP_MSG = ("Downloading directory: `{dir_name}`"
+UPLOADING_DIRECTORY_MSG = "Uploading [D]: `{dir_name}`..."
+DOWNLOADING_FILE_MSG = "Downloading [F]: `{file_name}`..."
+UPLOADING_FILE_MSG = "Uploading [F]: `{file_name}`..."
+DOWNLOADING_DIR_AS_ZIP_MSG = ("Downloading [D]: `{dir_name}`"
                               " as `{file_name}`...")
-SKIP_G_SUITE_FILE_MSG = "Skipping G.Suite file: `{file_name}` ..."
+SKIP_G_SUITE_FILE_MSG = "Skipping [S]: `{file_name}` ..."
