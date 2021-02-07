eaef64b79b0966b5702975ae1943599b4fe266c3
ddqof <dmitry.podaruev@urfu.me>
2021-02-07 22:10:06 +0500

    Refactor test_base_wrapper.py

--- tests/test_base_wrapper.py
+++ tests/test_base_wrapper.py
@@ -1,40 +1,56 @@
 import pytest
-from unittest.mock import Mock, patch
-
+from unittest.mock import Mock, patch, call
 from _base_wrapper import BaseWrapper
 
 
 @pytest.fixture()
 @patch.multiple(BaseWrapper, __abstractmethods__=set())
 def wrapper():
+    """
+    patch.multiple required for being able to initiate abstract class.
+    """
     return BaseWrapper(Mock())
 
 
-def test_put_file(wrapper, tmpdir, capsys):
-    test_file = tmpdir.join("test.txt")
-    test_file.write_binary(b"hello from test file")
-    wrapper._storage.get_upload_link.return_value = "upload link"
-    wrapper._put_file(test_file.strpath, "root")
-    wrapper._storage.get_upload_link.assert_called_once_with(
-        test_file.strpath, "root"
-    )
-    wrapper._storage.upload_file.assert_called_once_with(
-        "upload link", b"hello from test file"
-    )
-    captured = capsys.readouterr()
-    assert captured.out == f"Uploading: `{test_file.strpath}`...\n"
-
-
-def test_remove(wrapper, capsys):
+@pytest.fixture()
+def remote_file():
     file = Mock()
     file.name = "test_file"
-    wrapper._storage.get_file.return_value = file
+    return file
+
+
+def test_put_file_gets_upload_link_and_then_uploads(wrapper, tmp_path):
+    test_file = tmp_path / "test.txt"
+    test_file.write_bytes(b"hello from test file")
+    wrapper._storage.get_upload_link = Mock(return_value="upload link")
+    wrapper._storage.upload_file = Mock()
+    wrapper._put_file(test_file, "root")
+    assert wrapper._storage.get_upload_link.mock_calls == [
+        call(test_file, "root")
+    ]
+    assert wrapper._storage.upload_file.mock_calls == [
+        call("upload link", b"hello from test file")
+    ]
+
+
+def test_remove_prints_correct_data(wrapper, capsys, remote_file):
+    wrapper._storage.get_file.return_value = remote_file
     with patch("builtins.input") as input_mock:
         input_mock.return_value = "y"
         wrapper.remove("some_id", permanently=False)
-        wrapper._storage.remove.assert_called_once_with(
-            "some_id", permanently=False
-        )
-        input_mock.assert_called_once_with(
-            "Are you sure you want to permanently delete `test_file`? ([y]/n) "
-        )
+        assert input_mock.mock_calls == [
+            call(
+                "Are you sure you want to permanently delete "
+                "`test_file`? ([y]/n) "
+            )
+        ]
+
+
+def test_remove_call_storage_remove_properly(wrapper, remote_file):
+    wrapper._storage.get_file.return_value = remote_file
+    with patch("builtins.input") as input_mock:
+        input_mock.return_value = "y"
+        wrapper.remove("some_id", permanently=False)
+        assert wrapper._storage.remove.mock_calls == [
+            call("some_id", permanently=False)
+        ]
